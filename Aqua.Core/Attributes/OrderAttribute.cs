b3bb6cde135d61182878ba861fb1c58d5317eb88
IgoR-NiK <Igor_2501@mail.ru>
2021-03-27 14:49:49 +0300

    Fix OrderAttribute

--- Aqua.Core/Attributes/OrderAttribute.cs
+++ Aqua.Core/Attributes/OrderAttribute.cs
@@ -5,5 +5,11 @@ namespace Aqua.Core.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public class OrderAttribute : Attribute
     {
+        public int Value { get; }
+        
+        public OrderAttribute(int value)
+        {
+            Value = value;
+        }
     }
 }
