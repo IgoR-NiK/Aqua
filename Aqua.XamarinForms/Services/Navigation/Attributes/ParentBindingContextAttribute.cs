c32009774896c7b41d2d6e7ebb6d1ba628824285
IgoR-NiK <Igor_2501@mail.ru>
2021-03-27 03:13:36 +0300

    ParentBindingContextAttribute Non Inherited

--- Aqua.XamarinForms/Services/Navigation/Attributes/ParentBindingContextAttribute.cs
+++ Aqua.XamarinForms/Services/Navigation/Attributes/ParentBindingContextAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace Aqua.XamarinForms.Services.Navigation.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class ParentBindingContextAttribute : Attribute
     {
     }
