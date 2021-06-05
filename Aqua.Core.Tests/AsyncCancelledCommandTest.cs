3afa12b6ab180a4f214eaf555d670ce1b25066cb
IgoR-NiK <Igor_2501@mail.ru>
2021-06-05 20:48:25 +0300

    Fix AsyncCancelledCommandTest.cs

--- Aqua.Core.Tests/AsyncCancelledCommandTest.cs
+++ Aqua.Core.Tests/AsyncCancelledCommandTest.cs
@@ -16,13 +16,13 @@ namespace Aqua.Core.Tests
             var logger = new Logger();
             var command = new AsyncCancelledCommand(Execute, Cancelled);
 
-            // Команда выполняется 10 секунд и что-то асинхронно делает
+            // Команда выполняется 4 секунды и что-то асинхронно делает
             // По окончании пишем в логгер об успехе
             Task Execute(CancellationToken token)
                 => Task.Run(async () =>
                 {
-                    for (var i = 0; i < 10; i++)
-                        await Task.Delay(1000, token);
+                    for (var i = 0; i < 40; i++)
+                        await Task.Delay(100, token);
 
                     logger.Log("Command completed successfully");
                 }, token);
@@ -34,14 +34,14 @@ namespace Aqua.Core.Tests
             // Запускаем команду
             command.ExecuteAsync();
             
-            // Ждем 4 секунды
-            Thread.Sleep(4000);
+            // Ждем 1 секунду
+            Thread.Sleep(1000);
             
             // Недождались, отменяем
             command.Cancel();
             
-            // Ждем еще несколько секунд, чтобы успело сгенерироваться и обработаться исключение
-            Thread.Sleep(2000);
+            // Ждем еще немного, чтобы успело сгенерироваться и обработаться исключение
+            Thread.Sleep(500);
             
             Assert.AreEqual(1, logger.Logs.Count);
             Assert.AreEqual("Command canceled", logger.Logs[0]);
@@ -52,16 +52,16 @@ namespace Aqua.Core.Tests
         {
             var logger = new Logger();
             
-            // Даем команде 4 секунды на выполнение
-            var command = new AsyncCancelledCommand(Execute, Cancelled).WithTimeout(4000);
+            // Даем команде 2 секунды на выполнение
+            var command = new AsyncCancelledCommand(Execute, Cancelled).WithTimeout(2000);
 
-            // Команда выполняется 10 секунд и что-то асинхронно делает
+            // Команда выполняется 4 секунды и что-то асинхронно делает
             // По окончании пишем в логгер об успехе
             Task Execute(CancellationToken token)
                 => Task.Run(async () =>
                 {
-                    for (var i = 0; i < 10; i++)
-                        await Task.Delay(1000, token);
+                    for (var i = 0; i < 40; i++)
+                        await Task.Delay(100, token);
 
                     logger.Log("Command completed successfully");
                 }, token);
@@ -82,16 +82,16 @@ namespace Aqua.Core.Tests
         {
             var logger = new Logger();
             
-            // Даем команде 15 секунд на выполнение
-            var command = new AsyncCancelledCommand(Execute, Cancelled).WithTimeout(15000);
+            // Даем команде 5 секунд на выполнение
+            var command = new AsyncCancelledCommand(Execute, Cancelled).WithTimeout(5000);
 
-            // Команда выполняется 10 секунд и что-то асинхронно делает
+            // Команда выполняется 4 секунды и что-то асинхронно делает
             // По окончании пишем в логгер об успехе
             Task Execute(CancellationToken token)
                 => Task.Run(async () =>
                 {
-                    for (var i = 0; i < 10; i++)
-                        await Task.Delay(1000, token);
+                    for (var i = 0; i < 40; i++)
+                        await Task.Delay(100, token);
 
                     logger.Log("Command completed successfully");
                 }, token);
