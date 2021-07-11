9d6c9725e188257e533bc5f6c52747c38c01618a
IgoR-NiK <Igor_2501@mail.ru>
2021-07-11 14:43:56 +0300

    Fix ObservableCollectionExtension.cs

--- Aqua.Core/Aqua.Core/Utils/Extensions/ObservableCollectionExtension.cs
+++ Aqua.Core/Aqua.Core/Utils/Extensions/ObservableCollectionExtension.cs
@@ -12,11 +12,6 @@ namespace Aqua.Core.Utils
             => @this.AddRange(items);
 
         public static void AddRange<T>(this ObservableCollection<T> @this, IEnumerable<T> items)
-        {
-            foreach (var item in items)
-            {
-                @this.Add(item);
-            }
-        }
+            => items.ForEach(@this.Add);
     }
 }
