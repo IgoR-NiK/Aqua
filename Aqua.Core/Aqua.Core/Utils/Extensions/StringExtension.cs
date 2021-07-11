219d73499af5d8e9bc3b67bf18083cfbe1f67ac0
IgoR-NiK <Igor_2501@mail.ru>
2021-07-11 14:44:11 +0300

    Add IsNullOrWhiteSpace to StringExtension.cs

--- Aqua.Core/Aqua.Core/Utils/Extensions/StringExtension.cs
+++ Aqua.Core/Aqua.Core/Utils/Extensions/StringExtension.cs
@@ -4,5 +4,8 @@
     {
         public static bool IsNullOrEmpty(this string text) 
             => string.IsNullOrEmpty(text);
+
+        public static bool IsNullOrWhiteSpace(this string text)
+            => string.IsNullOrWhiteSpace(text);
     }
 }
