91a76b6de9912f0ba023eeb670bc5e0770438d9f
IgoR-NiK <Igor_2501@mail.ru>
2021-10-17 00:04:25 +0300

    Fix IWithResult{T1}.cs

--- Aqua.Core/Aqua.Core/Mvvm/Interfaces/IViewModel.cs
+++ Aqua.Core/Aqua.Core/Mvvm/Interfaces/IViewModel.cs
@@ -5,6 +5,8 @@ namespace Aqua.Core.Mvvm
 {
     public interface IViewModel : IResolvable
     {
+        string Title { get; set; }
+        
         void OnAppearing();
         
         Task OnAppearingAsync();
--- Aqua.Core/Aqua.Core/Utils/Interfaces/IWithResult{T1}.cs
+++ Aqua.Core/Aqua.Core/Utils/Interfaces/IWithResult{T1}.cs
@@ -1,6 +1,7 @@
-﻿namespace Aqua.Core.Services
+﻿namespace Aqua.Core.Utils
 {
     public interface IWithResult<out TResult>
     {
+        TResult Result { get; }
     }
 }
